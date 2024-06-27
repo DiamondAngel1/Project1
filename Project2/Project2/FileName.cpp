6ceceafea46e67e8837d7af2236a26fb76471b47
DiamondAngel1 <tararukolga78@gmail.com>
2024-06-27 17:55:40 +0300

    Create function UserInput

--- Project2/Project2/FileName.cpp
+++ Project2/Project2/FileName.cpp
@@ -2,8 +2,17 @@
 #include <Windows.h>
 using namespace std;
 
+void UserInput(char* str, int size) {
+    cout << "¬вед≥ть вираз: ";
+    cin.getline(str, size);
+}
+
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+
+    const int Size = 100;
+    char user[Size];
+    UserInput(user, Size);
 
 }
